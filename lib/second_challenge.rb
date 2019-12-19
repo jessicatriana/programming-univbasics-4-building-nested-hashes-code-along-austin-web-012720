256b976b4e1eebe288897eed374f0f80392120a8
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-19 19:30:41 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,11 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      patriarch: {
+        name: "Lord Montague",
+        age: "53",
+      }
+      :matriarch
       }, 
     :capulet => {
       #your key/value pairs here!
